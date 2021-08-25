be82fd217283dd5fdfbf7275cabd80abcd8eeb60
jlholm <jlholmz@gmail.com>
2021-08-24 21:50:06 -0400

    polynomal/datagram: remove TODO about clock precision

--- lib/polynomal/datagram.rb
+++ lib/polynomal/datagram.rb
@@ -16,18 +16,6 @@ module Polynomal
 
       def generate_generic_datagram(name, value, type)
         {
-          # TODO: Is there a more precise time that we can get?
-          #
-          # Such as:
-          #   Process.get_clocktime(Process::CLOCK_MONOTONIC)
-          #   -- or --
-          #   Process.get_clocktime(Process::CLOCK_REALTIME)
-          #
-          # Questions about `Time.now`:
-          #   - Does `Time.now` have drift/float?
-          #     * Apparently it uses Process::CLOCK_MONOTONIC under
-          #       the hood for *nix-based operating systems
-          #   - Is it subject to NTP adjustments?
           timestamp: Time.now.utc.to_i,
           type: type,
           name: normalize_name(name),
